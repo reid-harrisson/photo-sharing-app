babeaf88fece6c8eac41f9fb898daf7b22bb541d
Reid Harrison <reid.al.harrisson@gmail.com>
2025-01-17 10:25:50 +0100

    fix: delete unnecessary files

--- src/views/Chatroom/index.tsx
+++ src/views/Chatroom/index.tsx
@@ -11,7 +11,7 @@ import {
   Div,
   P,
 } from './styles';
-import { Screen } from 'components/Modal/styles';
+import { Screen } from 'components/modals/Modal/styles';
 import { UserListItem } from './UserListItem';
 import { UserListItemAvatar } from './UserListItem/styles';
 import { MessageListItem } from './MessageListItem/indext';
