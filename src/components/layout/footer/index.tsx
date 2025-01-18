4fcf1b3b53ac845bbfffd267c79fc50e5f7f503b
Reid Harrison <reid.al.harrisson@gmail.com>
2025-01-18 16:31:12 +0100

    fix: refactor footer

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,12 +1,56 @@
 import { Logo } from 'components/logo';
-import { Container } from './styles';
-import { COLORSTYLE } from 'consts';
+import {
+  Container,
+  Nav,
+  Content,
+  Wrapper,
+  Description,
+  Divider,
+} from './styles';
+import { COLORSTYLE, PATH } from 'consts';
+import { TextButton } from 'components';
+import { useNavigate } from 'react-router-dom';
 
-export const FooterComponent: React.FC = () => {
+const FOOTER_TEXT = {
+  description:
+    'FotOz is a user-friendly photo-sharing app that allows you to capture, edit, and share stunning images while connecting with a global community of photography lovers.',
+  copyright: 'Copyright @ FotOz',
+  navigation: {
+    community: 'Community',
+    chatting: 'Chatting',
+  },
+  additionalInfo:
+    'Photo sharing fosters connections among friends and family, enhancing relationships through shared experiences. It allows individuals to express their creativity and showcase their unique perspectives. Additionally, it serves as a way to preserve memories, making it easy to revisit significant moments in life.',
+} as const;
+
+export const Footer = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <Logo style={COLORSTYLE.LIGHT} />
-      <p>thanks for visiting photoshare</p>
+      <Wrapper>
+        <Content>
+          <Logo style={COLORSTYLE.LIGHT} />
+          <p>{FOOTER_TEXT.description}</p>
+          <small>{FOOTER_TEXT.copyright}</small>
+        </Content>
+        <Nav>
+          <TextButton
+            onClick={() => navigate(PATH.COMMUNITY)}
+            style={COLORSTYLE.LIGHT}
+          >
+            {FOOTER_TEXT.navigation.community}
+          </TextButton>
+          <TextButton
+            onClick={() => navigate(PATH.CHATROOM)}
+            style={COLORSTYLE.LIGHT}
+          >
+            {FOOTER_TEXT.navigation.chatting}
+          </TextButton>
+        </Nav>
+      </Wrapper>
+      <Divider />
+      <Description>{FOOTER_TEXT.additionalInfo}</Description>
     </Container>
   );
 };
--- src/components/layout/footer/styles.tsx
+++ src/components/layout/footer/styles.tsx
@@ -1,14 +1,42 @@
 import styled from 'styled-components';
+import { THEME } from 'consts';
 
 export const Container = styled.footer`
-  color: white;
-  background-color: #333333;
-  padding: 2rem;
-  box-shadow: 0 0 5px 0px black;
-
-  gap: 0.5rem;
-
+  color: ${THEME.COLORS.TEXT.LIGHT};
+  background-color: ${THEME.COLORS.BACKGROUND.DARK};
+  padding: 40px;
+  box-shadow: ${THEME.SHADOW.DEFAULT};
   display: flex;
   flex-direction: column;
-  align-items: center;
+  gap: 40px;
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const Description = styled.p`
+  margin: 0;
+`;
+
+export const Divider = styled.hr`
+  width: 100%;
+  border: none;
+  border-top: 1px solid ${THEME.COLORS.TEXT.LIGHT}40;
+  margin: 0;
 `;
